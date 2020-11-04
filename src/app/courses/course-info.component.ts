8af247562a988b90f1a8000b5601d2cce3a70c5b
yvpaulo@yahoo.com.br <yvpaulo@yahoo.com.br>
2020-11-04 11:49:33 -0300

    testando rotas e passagem de parametros por rotas

--- src/app/courses/course-info.component.ts
+++ src/app/courses/course-info.component.ts
@@ -9,15 +9,21 @@ import { CourseService } from './course.service';
 export class CourseInfoComponent implements OnInit {
 
     course: Course;
+    courseId: number;
 
     constructor(private activatedRoute: ActivatedRoute, private courseService: CourseService) { }
     
-    ngOnInit(): void { 
+
+    ngOnInit(): void {
+        this.courseId = +this.activatedRoute.snapshot.paramMap.get('id');
+
+    }
+    /*ngOnInit(): void { 
         this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe({
             next: course => this.course = course,
             error: err => console.log('Error', err)
         });
-    }
+    }*/
 
     save(): void {
         this.courseService.save(this.course).subscribe({
